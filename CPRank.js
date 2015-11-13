d8740da76621296512832efb73eeeaf4ca53a50d
Drybones3363 <adam.galauner@yahoo.com>
2015-11-13 13:03:20 -0500

    Code
    Use on scoreboard to find your team's rank and the number of teams competing.
    var code = your team code after "08-"

---
+++ CPRank.js
@@ -0,0 +1,10 @@
+var code = "1459";
+var it = document.getElementsByClassName("clickable");
+
+console.log("Teams: "+(it.length-6));
+
+for (var i=0;i<it.length;i++) {
+  if (it[i].getAttribute("href") == "team.php?team=08-"+code) {
+    console.log("We are place number "+(i-6)+"!");
+  }
+}
