da9b95fd200e8b8ecdd996af0d96a6f439b36929
Adam Galauner <adam.galauner@yahoo.com>
2016-01-29 13:08:03 -0500

    Update CPRank.js

--- CPRank.js
+++ CPRank.js
@@ -1,10 +1,10 @@
 var code = "1459";
 var it = document.getElementsByClassName("clickable");
 
-console.log("Teams: "+(it.length-5));
+console.log("Teams: "+(it.length-7));
 
 for (var i=0;i<it.length;i++) {
   if (it[i].getAttribute("href") == "team.php?team=08-"+code) {
-    console.log("We are place number "+(i-5)+"!");
+    console.log("We are place number "+(i-7)+"!");
   }
 }
